c2b2d972a62076a0b766224081974f051197bf61
RaulLeon01 <20243rd013@utez.edu.mx>
2025-05-08 11:43:18 -0600

    Programa que registra nombre

---
+++ Cadenas.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Cadenas {
+    public static void main(String[] args) {
+        //Crear un objeto que escanea el nombre del usuario ingresado desde teclado
+        Scanner almacenar = new Scanner(System.in);
+
+        System.out.println("Escribe tu nombre: ");
+        String nombre = almacenar.nextLine(); //Alamacena cadenas (nexLine)
+
+        System.out.println("El nombre es: " + nombre); //Imprime el nombre ingresado por el usuario
+
+    }
+}
